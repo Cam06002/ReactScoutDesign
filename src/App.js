8f1884fd044de3e5ec22fd69302f25c79feea1dd
Jeff Campbell <jeffcampbell333@gmail.com>
2023-02-08 19:15:17 -0800

    added router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Router, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
-function App() {
+export default function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello World</div>
+    },
+  ]);
+
+  createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <ReactProvider router={router} />
+    </React.StrictMode>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +25,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
-
-export default App;
