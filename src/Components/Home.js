42dc17fb55f3384cd641120b2097e654f85dea78
Jeff Campbell <jeffcampbell333@gmail.com>
2023-02-20 21:20:17 -0800

    added text and syling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,16 +5,28 @@ export default function Home(){
 
     return(
         <>
-            <div className="div-section-25">
-                <Button className="home-top-button">I Need a Website</Button>
-                <Button className="home-top-button">I'm Price Checking</Button>
-                <Button className="home-top-button">Just Looking</Button>
+            <div className="div-section-dark">
+
             </div>
             <div className="div-section-25">
-                <h5>Small Business Specialists</h5>
+                <div className="light-box">
+                    <h2 className="blue-text">Small Business Specialists</h2>
+                </div>
+                <div classname="light-box">
+                    <p>Whether you are a long-established pillar of the community, 
+                        or just starting out on a new venture, our web design services are curated 
+                        to meet the needs of your small business. We will work with you to create a 
+                        website and marketing strategy to retain your current clientele and find new 
+                        customers. </p>
+                </div>
             </div>
             <div className="div-section-25">
-                <h5>Design from Experience</h5>
+                <div classname="light-box">
+                    <p>From serving as employees of businesses large and small, to running a few of our own, we understand you because we've been in your shoes. This experience helps us to build the best products for you and your customers.</p>
+                </div>
+                <div classname="light-box">
+                    <h2 className="blue-text">Design from Experience</h2>
+                </div>
             </div>
             <div className="div-section-25">
                 <h5>A Personal Approach</h5>
